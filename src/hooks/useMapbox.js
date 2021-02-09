f1545cd44cb4e583b70cf4bc0a0b6b752054d7d3
andresdev <andresmestra11a@gmail.com>
2021-02-09 13:37:19 -0500

    Refactorización useMapbox

--- src/hooks/useMapbox.js
+++ src/hooks/useMapbox.js
@@ -1,18 +1,51 @@
 import { useRef, useEffect, useState, useCallback } from 'react'
 import mapboxgl from 'mapbox-gl'
+import { v4 } from 'uuid'
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_KEY
 
 export const useMapbox = (puntoInicial) => {
 
   const mapaDiv = useRef();
   //Resive el nodo html donde se renderiza el mapa
-  const setRef = useCallback( (node) => {
+  const setRef = useCallback((node) => {
     mapaDiv.current = node;
-  },[])
+  }, [])
 
+
+  //Referencia a los marcadores
+  const marcadores = useRef({});
+
+
+  //Mapa y coords
   const mapa = useRef();
   const [coords, setCoords] = useState(puntoInicial)
 
+
+  //función para agregar marcadores
+  const addMarcador = useCallback((event) => {
+
+    const { lng, lat } = event?.lngLat;
+
+    const marker = new mapboxgl.Marker();
+    marker.id = v4() //TODO: si el marcador ya tiene id
+    marker
+      .setLngLat([lng, lat]) //Cordenadas donde se va establecer
+      .addTo(mapa.current) // Agregar el mapa
+      .setDraggable(true) //Que se pueda mover
+
+    marcadores.current[marker.id] = marker;
+
+    //Eschuchar movimientos del marcador
+    marker.on('drag', ({ target }) => {
+      const { id } = target;
+      const { lng, lat } = target.getLngLat()
+      
+      //TODO: emitir los cambios del marcador
+    })
+
+  }, [])
+
+  //Crear mapa
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapaDiv.current,
@@ -39,10 +72,17 @@ export const useMapbox = (puntoInicial) => {
 
   }, [])
 
+  //Agregar marcadores al hacer click
+  useEffect(() => {
+    mapa.current?.on('click', addMarcador )
+  }, [addMarcador])
+
 
   return {
     coords,
     setRef,
+    addMarcador,
+    marcadores,
   }
 
 }
